26a9851b23ef8be6a9ebcd78a95b752fe5836383
Peter Szatmary <peter.szatmary@codexa.sk>
2018-11-23 19:58:21 +0100

    deleted unused import

--- src/main/java/com/reality/finder/realityfinder/ApplicationProperties.java
+++ src/main/java/com/reality/finder/realityfinder/ApplicationProperties.java
@@ -4,8 +4,6 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Data
 @Component
 public class ApplicationProperties {
